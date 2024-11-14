07423f1474f1bf689c36a967346398fabf946bc9
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2024-11-14 12:06:50 +0000

    nqswap created

---
+++ process_nqswap.py
@@ -0,0 +1,24 @@
+from typing import Dict, List, Any
+import json
+
+my_nqswap: List[Dict] = []
+
+with open('nqswap/nqswap_terrible.jsonl', 'r') as file:
+    isq = False
+    for line in file:
+        line = line.strip()
+
+        dataline: Dict[str, Any] = json.loads(line)
+
+        if isq:
+            my_nqswap[-1]["question"] = dataline["context_string"]
+
+        else:
+            my_nqswap.append({
+                "context": dataline["context_string"],
+                "answers": [dataline["gold_answers"]]
+            })
+        isq = not isq
+
+with open("nqswap.json", 'w') as jf:
+    json.dump(my_nqswap, jf)
